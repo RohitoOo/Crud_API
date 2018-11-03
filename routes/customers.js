a52bf4bf8bdf0db38470f0635605d3d2157783a1
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-11-03 10:15:39 -0400

    Added Post Request

--- routes/customers.js
+++ routes/customers.js
@@ -1,11 +1,13 @@
 const errors = require('restify-errors');
 const Customer = require('../models/Customers')
-
+const mongoose = require('mongoose')
 
 module.exports = server => {
+
+    // Get Customers 
     server.get('/customers' , async (req,res, next) => {
         
-        // Old Method 
+        // Old Methods ( Promise.then OR Callbacks )
         // Customer.find({})
         // .then(customersFromDatabase => {
         //     console.log({customersFromDatabase})
@@ -15,7 +17,7 @@ module.exports = server => {
 
         try {
             const customersFromDatabase = await Customer.find({})
-            console.log({customersFromDatabase})
+            res.send({customersFromDatabase})
     
         }
 
@@ -23,8 +25,32 @@ module.exports = server => {
             // Handling Errors - Restify Errors 
             console.log(err)
         }
-       
+        next();
+    })
 
+    // Add Customers 
+
+    server.post('/customer' , async (req,res,next) => {
+
+        // Destructuring Req.Body Object 
+        const {name,email,balance} = req.body;
+
+        const customer = new Customer ({
+         name, 
+         email,
+         balance
+        })
+
+        try {
+             await customer.save()
+            res.send({
+                msg: "Customer Saved To Database",
+                customer
+            })
+            next();
+        } catch(err){
+            console.log(err)
+        }
 
         next();
     })
