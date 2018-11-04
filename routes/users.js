c33bab245062f21906aa586821f611daf0b79585
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-11-04 01:21:47 -0500

    Decode Using bcrypt Js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const restify = require('restify');
 const User = require('../models/Users');
 const bcrypt = require('bcryptjs');
+const errors = require('restify-errors');
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+
 module.exports = server => {
     server.post('/register', async (req,res,next) => {
         
@@ -11,9 +15,6 @@ module.exports = server => {
             email,
             password
         });
-
-        console.log("Not HASED",user.password)
-
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(user.password, salt, async (err, hash) => {
                 // Hash Password 
@@ -28,20 +29,45 @@ module.exports = server => {
                         res.send(201)
                         next();
                     }catch(err){
-                        console.log(err)
+                        return next(new errors.InternalErrors(err.message))
                     }
             })
 
-       
-
         })
+    })
 
 
+    server.post('/user', async (req,res,next) => {
 
-   
+       try {const user = await User.findOne({email: req.body.email})
+
+        bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
+            if(isMatch){
+                // Create JWT 
+
+                const token = jwt.sign(user.toJSON(), config.JWT_SECRET, {expiresIn: '15m'})
 
 
-       
+                const { iat, exp } = jwt.decode(token)
+
+                // Respond with Token
+
+                res.send({ iat, exp, token})
+
+                // res.send({
+                //     message: "User Found",
+                //     user
+                // })
+            }else {
+                res.send({
+                    message: "Authentication Failed"
+                })
+            }
+        })}catch(err){
+            return next(new errors.UnauthorizedError(err.message))
+
+        }
+        
 
     })
 }
